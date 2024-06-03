925f7de47443e95eeebfac613ef63b104ad6572b
Alp SARICA <alp.sarica@eteration.com>
2024-06-03 13:37:46 +0300

    added docs and fixed a typo

--- core/twin_services.py
+++ core/twin_services.py
@@ -40,10 +40,22 @@ class TwinServices():
         self.node.create_service(CoreTwin, f"{self.nname}/delete_telemetry", self.callback_delete_telemetry)
         self.node.create_service(CoreTwin, f"{self.nname}/get_internet_status", self.callback_get_internet_status)
 
-
-
     def callback_get_current_properties(self, request, response):
-        """ # TODO add docs."""
+        """
+        Callback function to handle requests for the current properties of the device's stack.
+
+        This method is used as a ROS service callback to provide the current properties of the device's stack.
+        It retrieves the properties by calling the `get_current_properties` method on the node, converts the properties
+        to a JSON string, and sets this JSON string as the response output.
+
+        Args:
+            request: The service request object. It is not used in this method.
+            response: The service response object. The `output` attribute of this object will be set to the JSON string 
+                    representing the current stack properties.
+
+        Returns:
+            response: The modified service response object with the `output` attribute set to the JSON string of the current stack properties.
+        """
         stack = self.node.get_current_properties()
         
         response.output = json.dumps(stack)
@@ -51,7 +63,20 @@ class TwinServices():
         return response
     
     def callback_get_stack_definition(self, request, response):
-        """ # TODO add docs."""
+        """
+        Service callback to get the stack definition for a given stack ID.
+
+        This method is a ROS 2 service callback that handles requests to retrieve the stack definition.
+        It extracts the stack ID from the request, retrieves the stack definition using the node's `get_stack_definition` method,
+        and sets the definition in the response.
+
+        Args:
+            request: The request object containing the input stack ID.
+            response: The response object to be populated with the stack definition.
+
+        Returns:
+            response: The response object with the output set to the JSON-encoded stack definition.
+        """
         stack_id = request.input
         definition = self.node.get_stack_definition(stack_id)
         
@@ -60,7 +85,20 @@ class TwinServices():
         return response
     
     def callback_set_current_stack(self, request, response):
-        """ # TODO add docs."""
+        """
+        Handles the service request to set the current stack of the device.
+
+        This callback method processes a service request to update the current stack configuration of the device.
+        It extracts the payload from the request, calls the `set_current_stack` method of the node to perform the update, 
+        and sets the status code of the operation in the response.
+
+        Args:
+            request: The service request containing the input payload.
+            response: The service response that will contain the status code of the operation.
+
+        Returns:
+            response: The response with the status code as a string in the output attribute.
+        """
         payload = request.input
         status_code = self.node.set_current_stack(payload)
 
@@ -69,7 +107,20 @@ class TwinServices():
         return response
     
     def callback_get_context(self, request, response):
-        """ # TODO add docs."""
+        """
+        Callback method to handle requests for the device context.
+
+        This method is invoked when a service request for the device context is received. It retrieves the context
+        information of the device using the `get_context` method of the node, serializes it into a JSON string,
+        and assigns it to the response's output field.
+
+        Args:
+            request: The service request object (not used in this method).
+            response: The service response object that will contain the context information.
+
+        Returns:
+            response: The service response object with the context information in the output field.
+        """
         context = self.node.get_context()
         
         response.output = json.dumps(context)
@@ -77,7 +128,20 @@ class TwinServices():
         return response
 
     def callback_register_device(self, request, response):
-        """ # TODO add docs."""
+        """
+        Registers the device with the twin server.
+
+        This method is invoked as a callback to handle a request for registering the device with the twin server.
+        It calls the 'register_device' method of the associated 'node' instance to perform the registration process.
+        The status code returned by the 'register_device' method is converted to a string and set as the output in the response object.
+
+        Args:
+            request: The request object containing input data.
+            response: The response object for returning output data.
+
+        Returns:
+            response: The response object containing the status code of the device registration attempt.
+        """
         status_code = self.node.register_device()
 
         response.output = str(status_code)
@@ -85,7 +149,20 @@ class TwinServices():
         return response
     
     def callback_get_registered_telemetries(self, request, response):
-        """ # TODO add docs."""
+        """
+        Retrieves registered telemetry properties from the twin server.
+
+        This method is a callback function designed to handle a request for retrieving registered telemetry properties from the twin server.
+        It invokes the 'get_telemetry' method of the associated 'node' instance to fetch the telemetry data.
+        The telemetry data is then assigned to the 'output' attribute of the response object, which is returned.
+
+        Args:
+            request: The request object containing input data.
+            response: The response object for returning output data.
+
+        Returns:
+            response: The response object containing the registered telemetry properties.
+        """
         telemetry_data = self.node.get_telemetry()
 
         response.output = telemetry_data
@@ -93,7 +170,21 @@ class TwinServices():
         return response
     
     def callback_register_telemetry(self, request, response):
-        """ # TODO add docs."""
+        """
+        Registers telemetry with the twin server.
+
+        This method is a callback function designed to handle a request for registering telemetry with the twin server.
+        It retrieves the payload from the input attribute of the request object.
+        The payload is then passed to the 'register_telemetry' method of the associated 'node' instance to perform the registration.
+        The status code returned by the 'register_telemetry' method is converted to a string and set as the output in the response object.
+
+        Args:
+            request: The request object containing input data.
+            response: The response object for returning output data.
+
+        Returns:
+            response: The response object containing the status code of the telemetry registration attempt.
+        """
         payload = request.input
         status_code = self.node.register_telemetry(payload)
 
@@ -102,16 +193,43 @@ class TwinServices():
         return response
 
     def callback_delete_telemetry(self, request, response):
-        """ # TODO add docs."""
+        """
+        Deletes telemetry from the twin server.
+
+        This method is a callback function designed to handle a request for deleting telemetry from the twin server.
+        It retrieves the payload from the input attribute of the request object.
+        The payload is then passed to the 'delete_telemetry' method of the associated 'node' instance to perform the deletion.
+        The status code returned by the 'delete_telemetry' method is converted to a string and set as the output in the response object.
+
+        Args:
+            request: The request object containing input data.
+            response: The response object for returning output data.
+
+        Returns:
+            response: The response object containing the status code of the telemetry deletion attempt.
+        """
         payload = request.input
-        status_code = self.node.delete_telemtry(payload)
+        status_code = self.node.delete_telemetry(payload)
 
         response.output = str(status_code)
 
         return response
     
     def callback_get_internet_status(self, request, response):
-        """" # TODO add docs."""       
+        """
+        Retrieves the internet connection status.
+
+        This method is a callback function designed to handle a request for retrieving the internet connection status.
+        It retrieves the internet status from the associated 'node' instance and converts it to a string.
+        The string representation of the internet status is set as the output in the response object, which is returned.
+
+        Args:
+            request: The request object containing input data.
+            response: The response object for returning output data.
+
+        Returns:
+            response: The response object containing the internet connection status.
+        """
         response.output = str(self.node.internet_status)
 
         return response
