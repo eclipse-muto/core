d4d895daad8494e254801d3fda534a0626159231
İbrahim Sel <ibrahim.sel@eteration.com>
2024-09-11 12:05:52 +0300

    update setup.py for the new package

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
-from setuptools import setup
 import os
 from glob import glob
+from setuptools import setup
 
 package_name = 'core'
 
 setup(
     name=package_name,
-    version='1.0.0',
-    packages=[package_name],
+    version='0.0.0',
+    packages=[
+        package_name,
+        f"{package_name}.model"
+    ],
     data_files=[
-        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
-        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "log"), glob("log/*.log")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Alp SARICA',
     maintainer_email='alp.sarica@eteration.com',
-    description='Eclipse Muto Core Package',
-    license='Eclipse Public License v2.0',
+    description="Eclipse Muto Core Package",
+    license='EPL',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
