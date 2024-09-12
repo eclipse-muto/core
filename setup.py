a488c4ae82fd77d046f42553ee7fb3ccfcd761ce
İbrahim Sel <ibrahim.sel@eteration.com>
2024-09-12 11:03:01 +0300

    revert unnecessary setup.py changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'core'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[
         package_name,
         f"{package_name}.model"
@@ -17,14 +17,13 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
-        (os.path.join("share", package_name, "log"), glob("log/*.log")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Alp SARICA',
     maintainer_email='alp.sarica@eteration.com',
     description="Eclipse Muto Core Package",
-    license='EPL',
+    license='Eclipse Public License v2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
